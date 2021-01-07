f2e139fcc15c5eafea62d0a43e6b92167e290c2f
lfyang <yangliangfu@picb.ac.cn>
2021-01-07 20:27:00 +0800

    分支代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+    /**
+     * 这是分支的代码，进行测试
+     * @param args
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
